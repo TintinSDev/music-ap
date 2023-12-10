1c6fb5234ee9ea3c0ee259c2133ed6a9bfa30273
Martin <mmaina290@gmail.com>
2023-12-10 08:38:04 +0300

    aded input bar

--- public/components/SearchBar.jsx
+++ public/components/SearchBar.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 
-export default function SearchBar() {
+export default function SearchBar(handleSearch) {
 
     return (
-        <div>
-            <h1>Search Bar</h1>
-        </div>
+        <div >
+        <input
+          type="text"
+          placeholder="Search your song"
+          onChange={handleSearch}
+        />
+        <i className="circular search link icon"></i>
+      </div>
     )
 }
