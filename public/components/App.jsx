adb173a243b19eb8177cbec16259c91a1cc43cd2
Martin <mmaina290@gmail.com>
2023-12-10 08:56:58 +0300

    added App.jsx component to handle Searchbar componet

---
+++ public/components/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+
+export default function App (){
+
+  
+
+
+  return (
+    <div>
+      <h1>Favorite Songs</h1>
+      
+        
+      
+    </div>
+  );
+};
+
