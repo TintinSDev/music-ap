07a86267a7a6ae763f7ddd4abfb02cce0d4fb77c
Martin <mmaina290@gmail.com>
2023-12-10 08:58:47 +0300

    function handleSearch to filter songs eneterd by user

--- public/components/App.jsx
+++ public/components/App.jsx
@@ -4,7 +4,14 @@ import SearchBar from './SearchBar';
 export default function App (){
   const [filteredSongs, setFilteredSongs] = useState([]);
   
-
+ function handleSearch(searchSong) {
+   
+    const allSongs = ['Song A', 'Song B', 'Song C', 'Song D'];
+    const filteredSongs = allSongs.filter(song =>
+      song.toLowerCase().includes(searchSong.toLowerCase())
+    );
+    setFilteredSongs(filteredSongs);
+  };
 
   return (
     <div>
