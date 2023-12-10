102b716d518d818b2e09e3983e7acd5b02322298
Martin <mmaina290@gmail.com>
2023-12-10 08:57:44 +0300

    added state

--- public/components/App.jsx
+++ public/components/App.jsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import SearchBar from './SearchBar';
 
 export default function App (){
-
+  const [filteredSongs, setFilteredSongs] = useState([]);
   
 
 
