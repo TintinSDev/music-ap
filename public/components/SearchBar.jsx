0900844e93b92e067291550f63fe102c4a116ca3
Martin <mmaina290@gmail.com>
2023-12-10 08:50:38 +0300

    add state

--- public/components/SearchBar.jsx
+++ public/components/SearchBar.jsx
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function SearchBar(handleSearch) {
+export default function SearchBar({ onSearch }) {
+    // intoduce state to track users search
+    const [searchSong, setSearchSong] = useState('')
 
+    // define a varibale handleChange that detects changes in the input
+
+    function handleChange(event) { 
+        setSearchSong(event.target.value)
+        onSearch(event.target.value)
+    }
     return (
-        <div >
-        <input
-          type="text"
-          placeholder="Search your song"
-          onChange={handleSearch}
-        />
-        <i className="circular search link icon"></i>
-      </div>
+        <div>
+      <label htmlFor="search">Search:</label>
+      <input
+        type="text"
+        id="search"
+        placeholder="Enter your favorite song or genre"
+        value={searchTerm}
+        onChange={handleChange}
+      />
+    </div>
     )
 }
